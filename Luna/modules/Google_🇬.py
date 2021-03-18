cbe66097dd8254c8572206aa9508de45c72a5945
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-18 16:37:20 +0530

    Update Google_🇬.py

--- Luna/modules/Google_🇬.py
+++ Luna/modules/Google_🇬.py
@@ -9,12 +9,14 @@ import re
 import aiohttp
 import urllib.request
 from urllib.parse import urlencode
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+from search_engine_parser import GoogleSearch
 
 import bs4
 import html2text
-import requests
 from bing_image_downloader import downloader
-from PIL import Image
 from telethon import *
 from telethon.tl import functions
 from telethon.tl import types
@@ -24,38 +26,40 @@ from Luna import *
 
 from Luna.events import register
 
+opener = urllib.request.build_opener()
+useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
+opener.addheaders = [("User-agent", useragent)]
+
 
 @register(pattern="^/google (.*)")
 async def _(event):
     if event.fwd_from:
         return
     
-    # SHOW_DESCRIPTION = False
-    # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
-    input_str = event.pattern_match.group(1)
-    input_url = "https://bots.shrimadhavuk.me/search/?"
-    headers = {"USER-AGENT": "UniBorgV3"}
-    async with aiohttp.ClientSession() as requests:
-        data = {
-            "q": input_str,
-            GOOGLE_SRCH_KEY: GOOGLE_SRCH_VALUE
-        }
-        reponse = await requests.get(
-            input_url + urlencode(data),
-            headers=headers
-        )
-        response = await reponse.json()
-    output_str = " "
-    for result in response["results"]:
-        text = result.get("title")
-        url = result.get("url")
-        description = result.get("description")
-        last = html2text.html2text(description)
-        output_str += "[{}]({})\n{}\n".format(text, url, last)
-    await event.reply(
-        "{}".format(output_str), link_preview=False, parse_mode="Markdown"
+    webevent = await event.reply("searching........")
+    match = event.pattern_match.group(1)
+    page = re.findall(r"page=\d+", match)
+    try:
+        page = page[0]
+        page = page.replace("page=", "")
+        match = match.replace("page=" + page[0], "")
+    except IndexError:
+        page = 1
+    search_args = (str(match), int(page))
+    gsearch = GoogleSearch()
+    gresults = await gsearch.async_search(*search_args)
+    msg = ""
+    for i in range(len(gresults["links"])):
+        try:
+            title = gresults["titles"][i]
+            link = gresults["links"][i]
+            desc = gresults["descriptions"][i]
+            msg += f"✴️[{title}]({link})\n**{desc}**\n\n"
+        except IndexError:
+            break
+    await webevent.edit(
+        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
     )
-
 
 @register(pattern="^/img (.*)")
 async def img_sampler(event):
@@ -66,7 +70,7 @@ async def img_sampler(event):
     jit = f'"{query}"'
     downloader.download(
         jit,
-        limit=5,
+        limit=4,
         output_dir="store",
         adult_filter_off=False,
         force_replace=False,
