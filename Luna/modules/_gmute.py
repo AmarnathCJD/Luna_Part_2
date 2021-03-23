2b862030af313b4a4a20fa4a0356b87e0517b5a5
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-23 19:18:07 +0530

    Update _gmute.py

--- Luna/modules/_gmute.py
+++ Luna/modules/_gmute.py
@@ -25,27 +25,12 @@ async def gmute(event):
 
 @register(pattern="^/ungmute ?(.*)")
 async def ungmute(event):
-    if event.sender_id:
+    if event.sender_id == OWNER_ID:
        pass
     else:
-       return
-    private = False
-    if event.fwd_from:
-        return
-    if event.is_private:
-        await event.reply("Unexpected issues or ugly errors may occur!")
-        private = True
+      return
     reply = await event.get_reply_message()
-
-    if event.pattern_match.group(1) is not None:
-        userid = event.pattern_match.group(1)
-    elif reply is not None:
-        userid = reply.sender_id
-    elif private is True:
-        userid = event.chat_id
-    else:
-        return await event.reply("Please reply to a user or add their username into the command to ungmute them.")
-    replied_user = await event.client(GetFullUserRequest(userid))
+    userid = reply.sender_id
     if not is_muted(userid, "gmute"):
         return await event.reply("This user is not gmuted")
     try:
