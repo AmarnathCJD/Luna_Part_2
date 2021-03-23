a5faa20beadada5c89727db92d2ec3683d35c75b
Amarnath C <amarnathcharichilil@gmail.com>
2021-03-23 19:16:55 +0530

    Update _gmute.py

--- Luna/modules/_gmute.py
+++ Luna/modules/_gmute.py
@@ -14,7 +14,6 @@ async def gmute(event):
       return
     reply = await event.get_reply_message()
     userid = reply.sender_id
-    await event.reply('hi')
     if is_muted(userid, "gmute"):
         return await event.reply("This user is already gmuted")
     try:
@@ -24,6 +23,37 @@ async def gmute(event):
     else:
         await event.reply("Successfully gmuted that person")
 
+@register(pattern="^/ungmute ?(.*)")
+async def ungmute(event):
+    if event.sender_id:
+       pass
+    else:
+       return
+    private = False
+    if event.fwd_from:
+        return
+    if event.is_private:
+        await event.reply("Unexpected issues or ugly errors may occur!")
+        private = True
+    reply = await event.get_reply_message()
+
+    if event.pattern_match.group(1) is not None:
+        userid = event.pattern_match.group(1)
+    elif reply is not None:
+        userid = reply.sender_id
+    elif private is True:
+        userid = event.chat_id
+    else:
+        return await event.reply("Please reply to a user or add their username into the command to ungmute them.")
+    replied_user = await event.client(GetFullUserRequest(userid))
+    if not is_muted(userid, "gmute"):
+        return await event.reply("This user is not gmuted")
+    try:
+        unmute(userid, "gmute")
+    except Exception as e:
+        await event.reply("Error occured!\nError is " + str(e))
+    else:
+        await event.reply("Successfully ungmuted that person")
 
 
 @tbot.on(events.NewMessage(pattern=None))
